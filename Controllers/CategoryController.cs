8b8035917add1f128dd86f19fc912627995fe12e
Gabriel110200 <gb_souza@id.uff.br>
2022-04-22 22:26:04 -0300

    validations and delete done 22/04/22

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -41,11 +41,80 @@ namespace WebApplication1.Controllers
            
               _db.Categories.Add(obj);
               _db.SaveChanges();
+              TempData["success"] = "Category created successfully";
               return RedirectToAction("Index");
 
             }
 
             return View(obj);
+        }   
+
+        //GET 
+
+        public IActionResult Delete(int? id)
+        {
+            if(id==null || id==0)
+            {
+                return NotFound();
+            }
+
+            var category = _db.Categories.Find(id); 
+
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully"; 
+            return RedirectToAction("Index");
         }
+
+        // GET
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id ==0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);   
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+           // var categoryFromDbFirst = _db.Categories.FirstOrDefault(c => c.Id == id);  
+
+
+            return View(categoryFromDb);
+        }
+
+        // POST
+        [HttpPost] 
+        [ValidateAntiForgeryToken] 
+
+        public IActionResult Edit(Category obj)
+        {   
+
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the name");
+            }
+
+            if(ModelState.IsValid)
+            {
+               _db.Categories.Update(obj);
+               _db.SaveChanges();
+               TempData["success"] = "Category updated successfully";
+
+                return RedirectToAction("Index");
+
+            }
+            return View(obj); 
+        }
+
+        
     }
 }
